875fb40e0da12578f1a1fddcc1eeded71dfa4776
Daniel Kim <realdankim@gmail.com>
2022-02-04 00:38:27 -0800

    Shooting and aiming function added. Cursor to crosshair changed

---
+++ Assets/Shooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public Camera cam;
+
+    Vector2 mousePos;
+
+    public float bulletForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        if(Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        Vector2 aimDir = (mousePos - rb.position);
+        rb.AddForce(aimDir.normalized * bulletForce, ForceMode2D.Impulse);
+    }
+}
