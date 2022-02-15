ff99b1cfdb9ebd8cc8420e92184f897a43dd1be9
Daniel Kim <realdankim@gmail.com>
2022-02-15 01:52:56 -0800

    Basic dash function added with cooldown

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public Rigidbody2D rb; 
+    public float moveSpeed = 5f;
+    
+    [SerializeField] private float dashSpeed = 26f;
+    [SerializeField] private float dashLength = 0.15f;
+    [SerializeField] private float dashCooldown = .3f;
 
-    Vector2 movement; 
+    private Vector2 moveInput; 
+    private float activeMoveSpeed;
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    void Start() {
+        activeMoveSpeed = moveSpeed;
+    }
 
     void Update() {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Dash();
+        }
+
+        // If currently dashing, decrement dash counter
+        if (dashCounter > 0) {
+            dashCounter -= Time.deltaTime; 
+
+            if (dashCounter <= 0) {
+                activeMoveSpeed = moveSpeed; // Reset move speed to normal
+                dashCoolCounter = dashCooldown; // Start cooldown timer
+            }
+        }
+
+        // If dash cooldown counting down, decrement counter each frame
+        if (dashCoolCounter > 0) {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 
     void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveInput * activeMoveSpeed * Time.fixedDeltaTime); // Handle actual movement independent of time
+
     }
     
+    private void Dash() {
+        if (dashCoolCounter <=0 && dashCounter <=0) { // Can only dash if cooldown ready and not currently dashing
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+        }
+    }
 }
